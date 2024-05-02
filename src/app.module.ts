b8fbb438c031e9cab4d56f6c3fc40f758b410129
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-02 09:36:59 -0500

    added modules to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { LoginModule } from './login/login.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, LoginModule, ProductModule, OrderModule],
   controllers: [],
   providers: [],
 })
