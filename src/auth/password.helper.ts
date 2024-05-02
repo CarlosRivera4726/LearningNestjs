c35af82b4113554512647be8681e76da6daa3d40
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-02 09:35:14 -0500

    change comparePassword to use async method

--- src/auth/password.helper.ts
+++ src/auth/password.helper.ts
@@ -4,6 +4,9 @@ export function encryptPassword(password: string): string {
   return bcrypt.hashSync(password, 10).toString();
 }
 
-export function comparePassword(password: string, hash: string): boolean {
-  return bcrypt.compareSync(password, hash);
+export async function comparePassword(
+  password: string,
+  hash: string,
+): Promise<boolean> {
+  return bcrypt.compare(password, hash);
 }
