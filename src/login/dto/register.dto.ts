fe3d18db64e06de465ad0d0132ef8dc79547dfe5
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-02 14:05:28 -0500

    deleted login

--- src/login/dto/register.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsBoolean } from 'class-validator';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
-
-export class RegisterDto extends CreateUserDto {
-  @IsBoolean()
-  seller?: boolean;
-}
