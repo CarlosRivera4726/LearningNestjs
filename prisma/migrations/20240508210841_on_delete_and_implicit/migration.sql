5dcd07c44c1b0cfa8deb61755858146d5c6093f6
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-08 16:09:41 -0500

    ondelete and implicit relations

---
+++ prisma/migrations/20240508210841_on_delete_and_implicit/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - You are about to drop the `categoriesonproducts` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `productorder` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `categoriesonproducts` DROP FOREIGN KEY `CategoriesOnProducts_categoryId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `categoriesonproducts` DROP FOREIGN KEY `CategoriesOnProducts_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `productorder` DROP FOREIGN KEY `ProductOrder_orderId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `productorder` DROP FOREIGN KEY `ProductOrder_productId_fkey`;
+
+-- DropTable
+DROP TABLE `categoriesonproducts`;
+
+-- DropTable
+DROP TABLE `productorder`;
+
+-- CreateTable
+CREATE TABLE `_CategoryToProduct` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_CategoryToProduct_AB_unique`(`A`, `B`),
+    INDEX `_CategoryToProduct_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_OrderToProduct` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_OrderToProduct_AB_unique`(`A`, `B`),
+    INDEX `_OrderToProduct_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `_CategoryToProduct` ADD CONSTRAINT `_CategoryToProduct_A_fkey` FOREIGN KEY (`A`) REFERENCES `Category`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_CategoryToProduct` ADD CONSTRAINT `_CategoryToProduct_B_fkey` FOREIGN KEY (`B`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_OrderToProduct` ADD CONSTRAINT `_OrderToProduct_A_fkey` FOREIGN KEY (`A`) REFERENCES `Order`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_OrderToProduct` ADD CONSTRAINT `_OrderToProduct_B_fkey` FOREIGN KEY (`B`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
